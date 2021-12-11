80f12bf95eb218aa840a6cfde5ad39125d994737
PenelopeJulia <92155225+PenelopeJulia@users.noreply.github.com>
2021-12-11 20:40:29 +0100

    Aufgabe 9 überarbeitet (funktionell)

--- Aufgabe9/toDo.js
+++ Aufgabe9/toDo.js
@@ -1,30 +1,66 @@
 var toDoList;
 (function (toDoList) {
     // HTML-ELemente
-    var taskbox = document.getElementById("taskbox");
-    var textfeld = document.getElementById("textfield");
-    var todoArray = [];
-    // Index-Variable für Counter
-    var index = 0;
-    taskbox.appendChild(textfeld);
-    // Bei Klick auf dass Plus-Zeichen wird Text in einem Array gespeichert und 
-    function addTaskClick() {
-        console.log("klick");
+    var addTaskBox = document.getElementById("addTaskBox");
+    var newDiv = document.createElement("div");
+    newDiv.textContent = "";
+    addTaskBox.appendChild(newDiv);
+    document.querySelector("#addTaskButton").addEventListener("click", addNewTask);
+    var input = document.querySelector("#textfield");
+    //Funktion, die bei Klick auf Plus-Button neues InputText-Element ans Div(addBoxDiv) hängt
+    function addNewTask() {
+        var textVonEingabefeld;
+        if (input.value != "")
+            textVonEingabefeld = input.value;
+        input.value = "";
+        var addTaskBox = document.getElementById("addTaskBox");
+        var newDiv = document.createElement("div");
+        newDiv.textContent = textVonEingabefeld;
+        newDiv.id = "newDivBox";
+        //Add event listener, when this clicked.
+        //Delete parent
+        //Create check box
+        var checkbox = document.createElement("input");
+        //Declare Type and add checkbox
+        checkbox.type = "checkbox";
+        checkbox.id = "checkbox";
+        //Insert into DOM.
+        newDiv === null || newDiv === void 0 ? void 0 : newDiv.appendChild(checkbox);
+        //Add Delete-Button
+        var deleteButton = document.createElement("button");
+        //Add id to Delete-button
+        deleteButton.className = "fas fa-times";
+        deleteButton.type = "button";
+        deleteButton.addEventListener("click", deleteTaskbox);
+        function deleteTaskbox() {
+            newDiv.parentElement.removeChild(newDiv);
+        }
+        newDiv === null || newDiv === void 0 ? void 0 : newDiv.appendChild(deleteButton);
+        addTaskBox === null || addTaskBox === void 0 ? void 0 : addTaskBox.appendChild(newDiv);
     }
-    function deleteTaskbox() {
-    }
-    //Objekt-Array für Taskbox erstellen (Eigenschaften und Fuktionen?)
-    var task = [
-        {},
-        {}
-    ];
-    //Taskbox-Zähler   //unvollständig
-    /*for (let index: number = 0; index < task.length; index++) {
-        console.log(task[index].name);
-    }
+    /*
+    // Bei Klick auf dass Plus-Zeichen wird Text in einem Array gespeichert und
+    function addTaskIfClick (): void {
+    console.log("klick");
+    
+    let taskBox: HTMLDivElement = document.getElementById("taskbox");
+    
     */
-    //Bei Klick auf den Add Task-Button wird die Funktion addtaskClick aufgerufen
-    document.querySelector("#addTaskButton").addEventListener("mousedown", addTaskClick);
-    //EnterTask
+    /*
+    let liste = document.createElement("li");
+    let inputValue = document.getElementById("textfield").value;
+    let text = document.createTextNode(inputValue);
+    
+    liste.appendChild(text);
+    
+    if (inputValue === "") {
+        alert("Write somthing!");
+    } else {
+        document.getElementById("list").appendChild(liste);
+    }
+    document.getElementById("myInput").value = "";
+    
+    let addTaskButton = document.getElementById("textfield");
+    */
 })(toDoList || (toDoList = {}));
 //# sourceMappingURL=toDo.js.map
--- Aufgabe9/toDo.ts
+++ Aufgabe9/toDo.ts
@@ -1,29 +1,120 @@
 namespace toDoList {
 
 // HTML-ELemente
-const taskbox: HTMLElement = document.getElementById ("taskbox");
-
-let textfeld: HTMLElement = document.getElementById("textfield");
-
-let todoArray = [];
-
-// Index-Variable für Counter
-let index: number = 0;
-
-taskbox.appendChild(textfeld);
+let addTaskBox: HTMLElement = document.getElementById("addTaskBox");
 
 
+let newDiv: HTMLElement = document.createElement("div");
+
+newDiv.textContent = "";
+
+addTaskBox.appendChild(newDiv);
+
+document.querySelector("#addTaskButton").addEventListener("click", addNewTask);
+
+let input: HTMLInputElement = document.querySelector("#textfield");
+
+//Funktion, die bei Klick auf Plus-Button neues InputText-Element ans Div(addBoxDiv) hängt
+
+function addNewTask (): void {
+
+    let textVonEingabefeld;
+
+    if (input.value != "") textVonEingabefeld = input.value;
+
+    input.value = "";
+
+    let addTaskBox = document.getElementById("addTaskBox");
+
+    let newDiv = document.createElement("div");
+
+    newDiv.textContent = textVonEingabefeld; 
+    newDiv.id = "newDivBox";
+
+    //Add event listener, when this clicked.
+    //Delete parent
+
+    //Create check box
+    let checkbox: HTMLInputElement = document.createElement("input");
+
+    //Declare Type and add checkbox
+    checkbox.type  = "checkbox";
+    checkbox.id = "checkbox";
+
+    //Insert into DOM.
+    newDiv?.appendChild(checkbox);
+    
+    //Add Delete-Button
+    let deleteButton: HTMLElement = document.createElement("button");
+
+    //Add id to Delete-button
+    deleteButton.className = "fas fa-times";
+    deleteButton.type = "button"
+
+    deleteButton.addEventListener("click", deleteTaskbox);
+
+    function deleteTaskbox (): void {
+        newDiv.parentElement.removeChild(newDiv);
+    }
+
+    newDiv?.appendChild(deleteButton);    
+
+    addTaskBox?.appendChild(newDiv);
+
+}
+
+
+ 
+
+
+
+/*
 // Bei Klick auf dass Plus-Zeichen wird Text in einem Array gespeichert und 
-function addTaskClick (): void {
+function addTaskIfClick (): void {
 console.log("klick");
 
-    
+let taskBox: HTMLDivElement = document.getElementById("taskbox");
+
+*/
+
+
+
+/*
+let liste = document.createElement("li");
+let inputValue = document.getElementById("textfield").value;
+let text = document.createTextNode(inputValue);
+
+liste.appendChild(text);
+
+if (inputValue === "") {
+    alert("Write somthing!");
+} else {
+    document.getElementById("list").appendChild(liste);
+}
+document.getElementById("myInput").value = "";
+
+let addTaskButton = document.getElementById("textfield");
+*/
+
+
+}
+
+
+
+/*
+taskbox.appendChild(textfeld);
+let inputTextfeld;
+
+let input: HTMLInputElement = document.querySelector("#textfield");
+if (input.value != "") inputTextfeld = input.value;
+input.value = "";
 
 }
 
 function deleteTaskbox (): void {
-
+    
 }
+
 //Interface für Objekt-Array erstellen
 interface taskboxEigenschaften {
 
@@ -46,12 +137,12 @@ let task: taskboxEigenschaften [] = [
 /*for (let index: number = 0; index < task.length; index++) {
     console.log(task[index].name);
 }
-*/
+
 
 
 //Bei Klick auf den Add Task-Button wird die Funktion addtaskClick aufgerufen
-document.querySelector("#addTaskButton").addEventListener("mousedown", addTaskClick);
-
+document.querySelector("#addTaskButton").addEventListener("mousedown", addTaskIfClick);
+*/
 //EnterTask
 
 
